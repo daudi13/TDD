df29f9e86bcf392fdadf569ef64a90539b7b2462
daudi13 <davouma13@gmail.com>
2022-05-27 16:33:43 +0300

    Add test for Solver.factorial for negative numbers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,4 +9,8 @@ describe Solver do
     it "should return 1 when 0 is factorized" do 
       expect(calc.factorial(0)).to eq 1
     end
+
+    it "should return an error when a negative number id factorized" do 
+      expect(calc.factorial(-1)).to raise_error("cannot factorialize a negative number")
+    end
 end
