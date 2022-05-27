752b8679bed35f1eba8f9b67bda3fa242e448023
BonkeGcobo <madixmicro@gmail.com>
2022-05-27 19:11:08 +0200

    write fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -1,9 +1,21 @@
 class Solver
   def factorial(n)
-    if (n==0)
-      return 1
+      #raise("cannot factorialize a negative number") unless n>=0
+      if (n==0)
+        return 1
+      else
+        return n*factorial(n-1)
+     end
+  end
+
+  def fizzbuzz(number)
+    if(number%5==0 && number%3==0)
+      return "fizzbuzz"
+    
+    elsif (number%5==0)
+      return "buzz"
     else
-      return n*factorial(n-1)
+      return number
     end
   end
 end
