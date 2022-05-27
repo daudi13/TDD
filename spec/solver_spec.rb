bb48c1e41ac372174f7641b9a6bdfb3b525a0d4e
daudi13 <davouma13@gmail.com>
2022-05-27 16:32:18 +0300

    Add test for Solver.factorial for positive numbers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,8 @@
 describe Solver do
   context 'With valid inputs' do
-    math = Solver.new
+    calc = Solver.new
+
+    it "should perform the factorial of any number between 0 and a positive number" do
+      expect(calc.factorial(5)).to eq 120
+    end
 end
